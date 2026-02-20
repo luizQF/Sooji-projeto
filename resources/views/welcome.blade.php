6ba3d5bf82851564e7bd133c2bd93203bc7d25fc
luizQF <luizfqueiroz1811@email.com>
2026-02-20 10:36:14 -0300

    inicio do projeto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem vindo</title>
+</head>
+<body>
+    <header>
+        Sooji
+    </header>
+    
+
+    <section>
+        <h1>Bem vindo ao Sooji</h1>
+        <p>
+            O Sooji é um sistema de gerenciamento de tarefas que ajuda você a organizar suas atividades diárias de forma eficiente. Com uma interface intuitiva e recursos poderosos, o Sooji permite que você crie, gerencie e acompanhe suas tarefas de maneira fácil e rápida. Seja para uso pessoal ou profissional, o Sooji é a ferramenta ideal para manter sua vida organizada e produtiva.
+        </p>
+    </section>
+
+
+    <footer>
+
+    </footer>
+</body>
+</html>
